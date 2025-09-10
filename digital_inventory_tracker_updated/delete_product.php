5500aa6f9ecf69c4e5e0fe291b95c1015bd24d2a
Habiba22Akter <habiba22akter39@gmail.com>
2025-09-10 19:41:19 +0600

    Delete digital_inventory_tracker_updated directory

--- digital_inventory_tracker_updated/delete_product.php
+++
@@ -1,8 +0,0 @@
-<?php 
-include('config.php');
-
-if (isset($_GET['id'])) { 
-    $id = $_GET['id']; 
-    $conn->query("DELETE FROM products WHERE id = $id"); 
-    }
-header("Location: products.php"); ?>
